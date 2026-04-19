d13cf15fe82b4fa2495911f9633bfd4228646c12
”Artem <26and06@gmail.com>
2026-04-19 10:07:06 +0300

    BookingConfirmedEvent.java was created, gitkeep was deleted

---
+++ common-module/src/main/java/org/envycorp/commonmodule/events/BookingConfirmedEvent.java
@@ -0,0 +1,24 @@
+package org.envycorp.commonmodule.events;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookingConfirmedEvent {
+    private UUID bookingId;
+    private UUID userId;
+    private UUID tripId;
+    private String bookingType;
+    private String details;
+    private LocalDateTime confirmedAt;
+    private String confirmationNumber;
+}
