ace901150fc8487b0ec1240880eafcb9aaa834ea
”Artem <26and06@gmail.com>
2026-04-19 22:12:26 +0300

    created application-test.yml and changed other files

--- notification-service/src/test/java/org/envycorp/notificationservice/NotificationServiceApplicationTests.java
+++ notification-service/src/test/java/org/envycorp/notificationservice/NotificationServiceApplicationTests.java
@@ -2,8 +2,10 @@ package org.envycorp.notificationservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 
 @SpringBootTest
+@EmbeddedKafka
 class NotificationServiceApplicationTests {
 
     @Test
