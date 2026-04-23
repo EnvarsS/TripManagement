6a069e1062dca53bebeb67c40ce73a397e58b0ed
Vladyslav <eddiedipper4228@gmail.com>
2026-04-23 14:44:34 +0200

    Changed db migration to add role column to user table and change passwords to theirs hashed version

--- user-service/src/main/resources/db/changelog/changes/002-create-user-table.sql
+++ user-service/src/main/resources/db/changelog/changes/002-create-user-table.sql
@@ -6,5 +6,6 @@ CREATE TABLE users (
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     hashed_password VARCHAR(255) NOT NULL,
+    role_id BIGINT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
--- user-service/src/main/resources/db/changelog/changes/004-init-users-table.sql
+++ user-service/src/main/resources/db/changelog/changes/004-init-users-table.sql
@@ -3,32 +3,32 @@
 
 INSERT INTO users (name, email, hashed_password, role_id) VALUES
 (
-    'Admin',
+    'Admin1',
     'admin@example.com',
-    'pass1',
+    '$2a$10$8IqjvC/gu6BlsDhqIs2TKu8I7sYUHtK0q/VZNReNlx0s0EYt0jQhK',
     (SELECT id FROM roles WHERE name = 'ADMIN')
-),
-(
-    'User1',
-    'user1@example.com',
-    'pass2',
-    (SELECT id FROM roles WHERE name = 'USER')
 ),
 (
     'User2',
     'user2@example.com',
-    'pass3',
+    '$2a$10$DXwDap14b0IOoXRiNIzga.W5Ih4bZyOClPxc6SXQp0KYYBXJZMFjm',
     (SELECT id FROM roles WHERE name = 'USER')
 ),
 (
     'User3',
     'user3@example.com',
-    'pass4',
+    '$2a$10$0aiUr0yZuch2wSBfvqX8SOTDEe0iLj/jHPIDeR3OJkiJ84yGQTeV2',
     (SELECT id FROM roles WHERE name = 'USER')
 ),
 (
     'User4',
     'user4@example.com',
-    'pass5',
+    '$2a$10$lWeU9TJzbpSFSs06tZ9kSO/SZBwf0Z1X73YF9aVeyIQICSaB62lZW',
+    (SELECT id FROM roles WHERE name = 'USER')
+),
+(
+    'User5',
+    'user5@example.com',
+    '$2a$10$HzzoLm2bcMutn1IQ/9KNbehAKeWU1stUEX423POCHFKMhiyg80pm.',
     (SELECT id FROM roles WHERE name = 'USER')
 );
